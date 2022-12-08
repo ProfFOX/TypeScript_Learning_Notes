d8fc488b3644647f2933926501c1dd2237883a4e
Rawool Vijay Anil <vrawool0@gmail.com>
2022-12-08 21:22:27 +0530

    Information regarding number, boolean and any datatype

--- dist/1l_variables_datatypes.js
+++ dist/1l_variables_datatypes.js
@@ -9,3 +9,18 @@ let l_name = "Last Name"; // initialization
 l_name.toUpperCase(); // one you add . (dot) next to any variable it will show the related functions
 console.log(l_name);
 // number
+let price = 90;
+let price1 = 90.5; // number works for int, float and double as well
+let price2 = 44; // typescript is smart enough to guess the datatype and it will stick to is 
+//price2 = '33'     -- even if we didn't specify the datatype for the variable still typescript will
+//                  -- throw an assignmet error saying 
+// boolean
+let isConnect = true; // : boolean is a typescript syntax
+// any
+// it is not a good practice to use any if we want to stop using any we can do it through 
+// noImplicitAny option in tsconfig.json
+let obj; // any will turn off the type chacking for this particular variable 
+function fun1() {
+    return "hello";
+}
+obj = fun1();
--- src/1l_variables_datatypes.ts
+++ src/1l_variables_datatypes.ts
@@ -13,3 +13,23 @@ l_name.toUpperCase() // one you add . (dot) next to any variable it will show th
 console.log(l_name)
 
 // number
+let price : number = 90
+let price1 : number = 90.5 // number works for int, float and double as well
+let price2 = 44 // typescript is smart enough to guess the datatype and it will stick to is 
+//price2 = '33'     -- even if we didn't specify the datatype for the variable still typescript will
+//                  -- throw an assignmet error saying 
+
+
+// boolean
+let isConnect : boolean = true // : boolean is a typescript syntax
+
+// any
+
+// it is not a good practice to use any if we want to stop using any we can do it through 
+// noImplicitAny option in tsconfig.json
+let obj : any // any will turn off the type chacking for this particular variable 
+function fun1()
+{
+    return "hello"
+}
+obj = fun1()
